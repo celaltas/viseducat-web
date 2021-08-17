eec6c0767e63467c2f87bd31c86f3abc26c678f9
celal <celal.tas123@gmail.com>
2021-08-17 16:38:15 +0300

    resolve conflict for edit and delete buttons

--- viseducat_lms/models/course_detail.py
+++ viseducat_lms/models/course_detail.py
@@ -1,22 +1,16 @@
 from odoo import models, fields, api, _
 
-class VmCourseCommentReply(models.Model):
-    _name = 'vm.course.comment.reply'
-
-    content = fields.Text('Comment')
-    comment_id = fields.Many2one('vm.course.comment', ondelete='cascade')
-    
-
-
-
-
-
-
-
+#
+# class VmCourseDetail(models.Model):
+#     _name = 'vm.course.detail'
+#
+#     course_id = fields.Many2one('vm.course')
+#     comment_id = fields.Many2one('vm.comment')
+#
 
 class VmCourseComment(models.Model):
     _name = 'vm.course.comment'
 
     content = fields.Text('Comment')
     course_id = fields.Many2one('vm.course', ondelete='cascade')
-    response_ids = fields.Many2many('vm.course.comment.reply', string='Comment Response')
+    response_ids = fields.Many2many('vm.course.comment', 'vm_course_comment_rel', 'content', 'user_id', 'created_date')
