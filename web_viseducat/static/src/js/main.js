9673fafb3497367796df1b31ff82622cb8e57309
Bera Yilmazyurt <sentinelprime33@gmail.com>
2021-08-25 13:56:38 +0300

    Test

--- web_viseducat/static/src/js/main.js
+++ web_viseducat/static/src/js/main.js
@@ -1,3 +1,18 @@
+var ajax;
+
+odoo.define('web_viseducat', function (require) {
+  "use strict";
+  ajax = require('web.ajax');
+  
+});
+
+
+
+
+
+
+
+
 // Nav elements
 
 //const navToggle = document.querySelector(".nav-toggle");
@@ -6,7 +21,6 @@
 // Tab elements
 
 const btns = document.querySelectorAll(".tab-btn");
-const articles = document.querySelectorAll(".cd-content");
 const courseDetails = document.querySelector("#course-details-article");
 
 
@@ -27,71 +41,43 @@ const courseDetails = document.querySelector("#course-details-article");
 
 // Tab
 
+const tabList = document.querySelectorAll(".tab-btn")
+const articles = document.querySelectorAll(".cd-content");
+var currentIndex = 0
+tabList.forEach(function(button, index){
 
+  button.addEventListener('click', (event)=>{
+    tabList[currentIndex].classList.remove('active')
+    articles[currentIndex].classList.remove('active')
+    event.target.classList.toggle("active")
+    articles[index].classList.add("active")
+    currentIndex = index
+      
 
-courseDetails.addEventListener("click", function (e) {
-  const id = e.target.dataset.id;
-
-  if (id) {
-
-    // remove active from other buttons
-
-    btns.forEach(function (btn) {
-      btn.classList.remove("active");
-      e.target.classList.add("active");
-    })
-
-    // hide other articles
-
-    articles.forEach(function (article) {
-      article.classList.remove("active");
-    })
-    const element = document.getElementById(id);
-    element.classList.add("active");
-  }
+    
+    
+  })
+  
 })
+
+
+
 
 // Course Content
 
 const allContent = document.querySelector("#course-content-schedule")
-
 const childrenList = allContent.children
 
 
-
-
-
-
 for (let i = 0; i < childrenList.length; i++) {
-
-
   if (i % 2 === 0) {
-
-
     childrenList[i].addEventListener("click", function () {
-
       childrenList[i + 1].classList.toggle("active-table")
-
       const plus = childrenList[i].firstElementChild.firstElementChild
       const minus = childrenList[i].firstElementChild.firstElementChild.nextSibling.nextSibling
-
-
-
       plus.classList.toggle("deactive-plus")
       minus.classList.toggle("active-minus")
-
-
-
-
-
-
-
-
-
-
     })
-
-
   }
 }
 
@@ -101,86 +87,88 @@ const vebtns = document.querySelectorAll(".visible")
 
 
 vebtns.forEach(function (vebtn) {
-
   vebtn.addEventListener("click", function () {
-
-
-
     if (vebtn.style.backgroundColor != "red") {
       vebtn.style.backgroundColor = "red"
       vebtn.style.width = "auto"
       vebtn.textContent = "Employees Only"
     }
-
-
-
     else {
       vebtn.textContent = "Visible"
       vebtn.style.backgroundColor = "green"
-
-
     }
-
-
-
   })
-
-
-
 })
 
 
-// Reply Comment
-
 
 const replyButtons = document.querySelectorAll(".reply-comment")
-const cancelButtons = document.querySelectorAll(".cancel-cmnt")
+const wrapperComment = document.querySelectorAll(".wrapper-comment")
 const ellipsisButtons = document.querySelectorAll(".ellipsis")
-const editDelete = document.querySelectorAll(".delete-edit")
+const editDeleteMenu = document.querySelectorAll(".delete-edit")
+const editBtn = document.querySelectorAll(".edit")
+const updateForm = document.querySelectorAll(".update-answer")
+const responseForm = document.querySelectorAll(".answered-comment")
+const cancelButton = document.querySelectorAll(".cancel-cmnt")
+const replyTextAreas = document.querySelectorAll(".answer-reply-cont")
+const updatePostBtn = document.querySelectorAll(".update-post-btn")
+const updatedTextArea = document.querySelectorAll("#update-textarea")
+const updateCancelBtn = document.querySelectorAll(".update-cancel-btn")
+const deleteBtn = document.querySelectorAll(".delete")
 
 
 
-for (let i = 0; i < replyButtons.length; i++) {
 
 
 
+for (let i = 0; i < wrapperComment.length; i++) {
 
+  replyButtons[i].addEventListener('click', ()=>{
+    replyTextAreas[i].classList.toggle("active")
+  })
 
-  const replyTextAreas = replyButtons[i].parentElement.parentElement.nextElementSibling
+  cancelButton[i].addEventListener('click', ()=>{
+    replyTextAreas[i].classList.toggle("active")
+  })
 
-
-  if (replyTextAreas.classList[1] === "answered-comment") {
+  const siblingList = wrapperComment[i].nextElementSibling
+  if (siblingList.classList.contains("answered-comment")){
     replyButtons[i].style.display = "none"
   }
 
-  replyButtons[i].addEventListener("click", function (event) {
+}
 
-    identify = event.target.id
+// these for comment-reply events
 
-    if (identify === replyTextAreas.id) {
-      replyTextAreas.classList.toggle("active")
-    }
+for (let index = 0; index < editBtn.length; index++) {
 
-
-
-
-
-
-    cancelButtons[i].addEventListener("click", function () {
-
-      replyTextAreas.classList.toggle("active")
-
-    })
+  ellipsisButtons[index].addEventListener('click', ()=>{
+    editDeleteMenu[index].classList.toggle("active")
   })
-  if (replyTextAreas.classList[1] === "answered-comment")
-    ellipsisButtons[i].addEventListener("click", function () {
 
-      editDelete[i].classList.toggle("active-flex")
+  editBtn[index].addEventListener('click', ()=>{
+    updateForm[index].classList.toggle("active")
+    responseForm[index].classList.toggle("deactive")
+  })
 
-    })
+  updateCancelBtn[index].addEventListener('click', ()=>{
+    updateForm[index].classList.remove("active")
+    responseForm[index].classList.toggle("active")
+    editDeleteMenu[index].classList.remove("active")
 
+  })
+  deleteBtn[index].addEventListener('click', ()=>{
+    const commentID = deleteBtn[index].getAttribute('data-id').split('/')[1]
+    deleteByAjax(commentID, index)
+  })
 
+  updatePostBtn[index].addEventListener('click', ()=>{
+    const commentID = deleteBtn[index].getAttribute('data-id').split('/')[1]
+    const updateText = updatedTextArea[index].value
+    editByAjax(commentID, updateText, index)
 
+  })
+  
 }
 
 
@@ -190,67 +178,73 @@ for (let i = 0; i < replyButtons.length; i++) {
 
 
 
-odoo.define('web_viseducat', function (require) {
-  "use strict";
-
-  const ajax = require('web.ajax');
-  const deleteBtn = document.querySelector(".delete")
-  const editBtn = document.querySelector(".edit")
-  const replyID = deleteBtn.getAttribute('data-id').split('/')[1]
-  deleteBtn.addEventListener('click', deleteByAjax)
-  editBtn.addEventListener('click', editByAjax)
-  
 
 
-  function deleteByAjax() {
-    console.log("delete butonu tıklandı")
-    const params = {
-      'id': replyID
-    }
-    
-    const url = '/delete'
-  
-    ajax.jsonRpc(url, 'call', params).then(function (data) {
-      
-      console.log("çağrı yapıldı")
-      console.log("data" + data.result)
 
-      if (data.result){
-        alert("Divi kapa")
-      }
-      else{
-        alert("Hata")
-      }
-  
-    }).catch(function (ex) {
-  
-      console.log("ex: " + ex)
-  
-  
-    });
+
+
+
+
+
+
+
+
+
+
+function deleteByAjax(id,index) {
+  const params = {
+    'id': id
   }
 
-  function editByAjax() {
-    console.log("edit butonu tıklandı")
-    const params = {
-      'id': replyID
+  const url = '/delete'
+
+  ajax.jsonRpc(url, 'call', params).then(function (data) {
+
+    if (data.result){
+      responseForm[index].classList.toggle("deactive")
+      replyButtons[index].style.display = "block"
     }
-    
-    const url = '/edit'
-  
-    ajax.jsonRpc(url, 'call', params).then(function (data) {
-      
-      console.log("edit çağrı yapıldı")
-      console.log("data" + data.content)
+    else{
+      console.log("An error occured!!!")
+    }
+
+  }).catch(function (ex) {
+
+    console.log("ex: " + ex)
 
 
-    }).catch(function (ex) {
-  
-      console.log("ex: " + ex)
-  
-  
-    });
+  });
+}
+
+
+function editByAjax(id,text, index) {
+  const params = {
+    'id': id,
+    'text': text,
   }
 
+  const url = '/edit'
 
-});
+  ajax.jsonRpc(url, 'call', params).then(function (data) {
+
+    if (data.result){
+      console.log("successful!!!")
+      const updateDate = new Date()
+      responseForm[index].getElementsByClassName("published-time")[0].textContent = "Updated on " +updateDate.toLocaleString()
+      responseForm[index].getElementsByClassName("comment-area")[0].textContent = text
+      updateForm[index].classList.remove("active")
+      responseForm[index].classList.toggle("active")
+      editDeleteMenu[index].classList.remove("active")
+    }
+    else{
+      console.log("An error occured!!!")
+    }
+
+  }).catch(function (ex) {
+
+    console.log("ex: " + ex)
+
+
+  });
+}
+
